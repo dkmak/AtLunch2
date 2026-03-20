a0232a0b15c62c000137b14b8a2f10f3eae78bf6
Darryl Mak <darrylmak2@gmail.com>
2026-03-19 21:47:40 -0400

    Revert "fix race condition between location and search"
    This reverts commit be5fe1ced80ef937d2e529e1b324f642103c9d4c.

--- app/src/main/java/com/atlunch/ui/listplaces/PlacesScreen.kt
+++ app/src/main/java/com/atlunch/ui/listplaces/PlacesScreen.kt
@@ -86,24 +86,30 @@ fun PlacesScreen(
     }
 
     LaunchedEffect(Unit) {
-        val hasLocationPermission = ContextCompat.checkSelfPermission(
-            context,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED ||
-            ContextCompat.checkSelfPermission(
+        if (textFieldValue.isEmpty()) {
+            val hasLocationPermission = ContextCompat.checkSelfPermission(
                 context,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-
-        viewModel.onLocationPermissionChanged(hasLocationPermission)
-
-        if (!hasLocationPermission) {
-            locationPermissionLauncher.launch(
-                arrayOf(
-                    Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(
+                    context,
                     Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+
+            viewModel.onLocationPermissionChanged(hasLocationPermission)
+
+            if (hasLocationPermission) {
+                viewModel.search(textFieldValue)
+            } else {
+                locationPermissionLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
                 )
-            )
+            }
+        } else {
+            viewModel.search(textFieldValue)
         }
     }
 
@@ -283,6 +289,7 @@ fun PlacesMapContent(
             GoogleMap(
                 modifier = Modifier.fillMaxSize(),
                 cameraPositionState = cameraPositionState,
+                onMapLoaded = { mapLoaded = true }
             ) {
                 placePreviews.filter { preview -> preview.location != null }.forEach {preview ->
                     val (previewLat, previewLong) = preview.location!!
